82270ed27cc51111524b7e4e1bb3b84e64a48458
laviathan101 <jean_recardo_phillips@hotmail.com>
2023-05-13 10:44:55 +0900

    Update _app.tsx

--- VtuberTwitchFinder.Client/src/pages/_app.tsx
+++ VtuberTwitchFinder.Client/src/pages/_app.tsx
@@ -4,13 +4,13 @@ import type {AppProps} from 'next/app'
 import {AxiosQuery} from "@/api";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-if (process.env.NEXT_PUBLIC_BASEURL)
-    AxiosQuery.setBaseUrl(process.env.NEXT_PUBLIC_BASEURL);
-else
-    console.warn("Failed to load base url");
 const queryClient = new QueryClient();
 
 export default function App({Component, pageProps}: AppProps) {
+    if (process.env.NEXT_PUBLIC_BASEURL)
+        AxiosQuery.setBaseUrl(process.env.NEXT_PUBLIC_BASEURL);
+    else
+        AxiosQuery.setBaseUrl("https://api.vtubers.app");
     return (
         <ChakraProvider>
             <QueryClientProvider client={queryClient}>
