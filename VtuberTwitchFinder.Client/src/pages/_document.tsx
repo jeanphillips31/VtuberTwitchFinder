2d594266ed10c5c32b4559a05c5f73d5c7f5d721
jeanphillips31 <jean_recardo_phillips@hotmail.com>
2025-02-17 20:58:35 +1100

    updated doc to include next script

--- VtuberTwitchFinder.Client/src/pages/_document.tsx
+++ VtuberTwitchFinder.Client/src/pages/_document.tsx
@@ -1,15 +1,22 @@
-import {Head, Html, Main, NextScript} from 'next/document'
-import {ColorModeScript, theme} from "@chakra-ui/react";
+import {Head, Html, Main, NextScript} from 'next/document';
+import {ColorModeScript} from "@chakra-ui/react";
+import Script from 'next/script';
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head/>
+            <Head>
+                <Script
+                    strategy="defer"
+                    data-domain="vtubers.app"
+                    src="https://plausible.plexus.gg/js/script.js"
+                />
+            </Head>
             <body>
             <ColorModeScript initialColorMode={"dark"}/>
             <Main/>
             <NextScript/>
             </body>
         </Html>
-    )
+    );
 }
